cf55a762592cb27b5cb166eb61a8a76cd0090343
Simon Wrafter <simon.wrafter@gmail.com>
2015-04-12 23:59:29 +0200

    dxfzgvv

--- ex2/task1.py
+++ ex2/task1.py
@@ -11,7 +11,7 @@ from  pylab import *
 ab0 = [0.7, 0.7]
 
 def F(a, b):
-    return array([5*cos(a) + 6*cos(a+b) - 10, 5*sin(a) + 6*sin(a+b) - 4])
+    return array([5*cos(a) + 6*cos(a+b) - 10., 5*sin(a) + 6*sin(a+b) - 4.])
 
 def Jack(F, xa, xb):
     eps = np.finfo(float).eps * 10000
@@ -21,17 +21,24 @@ def Jack(F, xa, xb):
 
     return array([[deriva[0], derivb[0]], [deriva[1], derivb[1]]])
 
-def noninvert(J, F):
+def noninvert(J, F, xn, xtol):
     G = identity(2) - J
     c = F
-    for i in
+    for i in range(2000):
+        xnp1 = G*xn + c
+        if abs(norm(xnp1-xn)) < xtol:
+            return xnp1
+        else:
+            xn = xnp1
+    else:
+        raise Exception("bajs")
 
 
 def newton(F, x0, xtol = 1e-4):
     xn = x0
     for i in range(2000):
         J = Jack(F, *xn)
-        xnp1 = xn - noninvert(J) * F(*xn)
+        xnp1 = xn - noninvert(J, F(*xn), xn, xtol) * F(*xn)
         if abs(norm(xn-xnp1)) < xtol:
             return xnp1, i, err_list, guess_list
         else:
